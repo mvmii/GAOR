ba9d410a83b02dbe7f02cabd1efbff4d51a3cb8f
Hua <mvmii1210@gmail.com>
2023-12-28 13:03:52 +0800

    添加抓取標籤h1練習

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import tkinter as tk
-from tkinter import Entry, Label, Button, Text, Scrollbar, messagebox
-import requests
-
-
 # 第一次練習爬蟲並建立一個GUI  (GPT教學)
 # 安裝指令  :  pip install requests  (庫)
+#            pip install beautifulsoup4 (來解析 HTML，並找到相應的元素)
 #             pip install pyinstaller (將py轉exe)
 #             pyinstaller --onefile main.py  (產exe)
 
@@ -23,6 +19,11 @@ import requests
 # wxPython是基于wxWidgets库的Python绑定，wxWidgets是一个用C++编写的跨平台GUI库。
 # wxPython提供了良好的跨平台性能，并具有相对现代化的外观。
 # 它是一个中等规模的库，介于Tkinter和PyQt之间，提供了足够的功能，同时也保持了一定的简单性。
+
+import tkinter as tk
+from tkinter import Entry, Label, Button, Text, Scrollbar, messagebox
+import requests
+from bs4 import BeautifulSoup
 
 class YourApplication:  # 模組化
     def __init__(self, master):
@@ -52,11 +53,27 @@ class YourApplication:  # 模組化
         self.scrollbar.pack(side="right", fill="y")
         self.result_text.config(yscrollcommand=self.scrollbar.set)
 
+        # Label顯示抓取的H1標籤內容
+        self.h1_label = Label(master, text="")
+        self.h1_label.pack()
+
+
+
     def fetch_chaotic_code(self):
         url = self.url_entry.get()
         try:
             response = requests.get(url)
             response.raise_for_status()  # 檢查是否有錯誤的HTTP響應
+
+            # 使用BeautifulSoup解析HTML
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # 找到第一個h1標籤
+            h1_tag = soup.find('h1')
+            # 取得h1標籤的內容，如果存在
+            h1_content = h1_tag.text if h1_tag else "未找到 H1 標籤"
+            # 更新Label的內容
+            self.h1_label.config(text=f"H1標籤內容：{h1_content}")
+
             chaotic_code = response.text
             self.result_text.delete(1.0, "end")
             self.result_text.insert("end", chaotic_code)
