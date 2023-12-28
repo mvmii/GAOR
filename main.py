569a1e712fb2c5b4029b9c33709565de663e181e
Hua <mvmii1210@gmail.com>
2023-12-28 14:04:28 +0800

    1.將requests移除，改用selenium selenium 會同步開啟瀏覽器 這是為了在爬蟲時可以查看瀏覽器進度到哪裡 目前註解這行 chrome_options.add_argument("--headless") 是因為這行代表不顯示瀏覽器做事

--- main.py
+++ main.py
@@ -3,27 +3,20 @@
 #            pip install beautifulsoup4 (來解析 HTML，並找到相應的元素)
 #             pip install pyinstaller (將py轉exe)
 #             pyinstaller --onefile main.py  (產exe)
+#             pip install selenium  (以訪客身分開啟 Chrome 並加載當前網址)
 
 # 簡單說明 GUI類型  目前先使用【Tkinter】來實作
-# Tkinter:
-# Tkinter是Python标准库中自带的GUI库，因此不需要额外安装。
-# 它提供了相对简单的接口，适合初学者。然而，它的外观相对较为简单，可能不如其他库提供的现代化外观。
-# Tkinter基于Tk GUI工具包，它是一个跨平台的工具包，但在某些平台上可能没有其他库那么好看。
-#
-# PyQt:
-# PyQt是基于Qt库的Python绑定，Qt是一个强大且成熟的C++ GUI库。
-# PyQt提供了丰富的功能和现代化的外观，支持大量的组件和特性。
-# PyQt是一个相对重量级的库，但也因此提供了更多的灵活性和功能。
-#
-# wxPython:
-# wxPython是基于wxWidgets库的Python绑定，wxWidgets是一个用C++编写的跨平台GUI库。
-# wxPython提供了良好的跨平台性能，并具有相对现代化的外观。
-# 它是一个中等规模的库，介于Tkinter和PyQt之间，提供了足够的功能，同时也保持了一定的简单性。
 
 import tkinter as tk
 from tkinter import Entry, Label, Button, Text, Scrollbar, messagebox
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+
+# 使用Selenium開啟Chrome
+chrome_options = Options()
+# chrome_options.add_argument("--headless")  # 在後台執行
+driver = webdriver.Chrome(options=chrome_options)
 
 class YourApplication:  # 模組化
     def __init__(self, master):
@@ -60,16 +53,17 @@ class YourApplication:  # 模組化
         self.h1_label = Label(master, text="")
         self.h1_label.pack()
 
-
-
     def fetch_chaotic_code(self):
         url = self.url_entry.get()
         try:
-            response = requests.get(url)
-            response.raise_for_status()  # 檢查是否有錯誤的HTTP響應
+            # 加載URL
+            driver.get(url)
+            # 取得當前網頁HTML
+            response_text = driver.page_source
 
             # 使用BeautifulSoup解析HTML
-            soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response_text, 'html.parser')
+
             # 找到第一個h1標籤
             h1_tag = soup.find('h1')
             # 取得h1標籤的內容，如果存在
@@ -77,10 +71,16 @@ class YourApplication:  # 模組化
             # 更新Label的內容
             self.h1_label.config(text=f"H1標籤內容：{h1_content}")
 
-            chaotic_code = response.text
+            # 顯示亂碼在Text中
             self.result_text.delete(1.0, "end")
-            self.result_text.insert("end", chaotic_code)
-        except requests.exceptions.RequestException as e:
+            self.result_text.insert("end", response_text)
+
+            # 關閉Chrome
+            # driver.quit()
+            # 關閉當前瀏覽器窗口
+            # driver.close()
+
+        except Exception as e:
             messagebox.showerror("錯誤", f"無法獲取亂碼：{e}")
 
 
